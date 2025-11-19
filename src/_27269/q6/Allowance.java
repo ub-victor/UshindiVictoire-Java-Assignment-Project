148d5adfc377e06917e748991a6b8c734b0f7fd3
ub-victor <victoireushindi371@gmail.com>
2025-11-19 17:14:30 +0200

    Inherintance

--- src/_27269/q6/Allowance.java
+++ src/_27269/q6/Allowance.java
@@ -1,5 +1,30 @@
 package _27269.q6;
 
-public class Allowance {
-    
+public class Allowance extends PayGrade {
+    private double housingAllowance;
+    private double transportAllowance;
+    private double otherAllowance;
+
+    public Allowance(int id, String createdDate, String updatedDate,
+                     String employeeName, String employeeTIN, String department,
+                     String contractType, String startDate, String endDate,
+                     String positionName, double baseSalary,
+                     String gradeName, double multiplier,
+                     double housingAllowance, double transportAllowance, double otherAllowance)
+            throws PayrollException {
+        super(id, createdDate, updatedDate, employeeName, employeeTIN, department,
+                contractType, startDate, endDate, positionName, baseSalary,
+                gradeName, multiplier);
+
+        if (housingAllowance < 0 || transportAllowance < 0 || otherAllowance < 0)
+            throw new PayrollException("allowances must be >= 0");
+
+        this.housingAllowance = housingAllowance;
+        this.transportAllowance = transportAllowance;
+        this.otherAllowance = otherAllowance;
+    }
+
+    public double getHousingAllowance() { return housingAllowance; }
+    public double getTransportAllowance() { return transportAllowance; }
+    public double getOtherAllowance() { return otherAllowance; }
 }
