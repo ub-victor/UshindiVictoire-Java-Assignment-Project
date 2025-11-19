a22397bda1b436073f63214983f237540dc97c66
ub-victor <victoireushindi371@gmail.com>
2025-11-19 17:11:13 +0200

    Inherintance

--- src/_27269/q6/PayrollException.java
+++ src/_27269/q6/PayrollException.java
@@ -1,5 +1,7 @@
 package _27269.q6;
 
-public class PayrollException {
-    
+public class PayrollException extends Exception {
+    public PayrollException(String message) {
+        super(message);
+    }
 }
