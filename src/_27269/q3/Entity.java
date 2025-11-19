ae9ea6dd910dbc45889364e08214a5d42551a715
ub-victor <victoireushindi371@gmail.com>
2025-11-19 16:12:54 +0200

    Inherintance

--- src/_27269/q3/Entity.java
+++ src/_27269/q3/Entity.java
@@ -1,5 +1,22 @@
-package _27269.q1;
+package _27269.q3;
 
 public class Entity {
-    
+    private int id;
+    private String createdDate;
+    private String updatedDate;
+
+    public Entity(int id, String createdDate, String updatedDate) throws TaxDataException {
+        if (id <= 0) throw new TaxDataException("ID must be > 0");
+        if (createdDate == null || createdDate.trim().isEmpty())
+            throw new TaxDataException("createdDate required");
+        if (updatedDate == null || updatedDate.trim().isEmpty())
+            throw new TaxDataException("updatedDate required");
+        this.id = id;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
+    }
+
+    public int getId() { return id; }
+    public String getCreatedDate() { return createdDate; }
+    public String getUpdatedDate() { return updatedDate; }
 }
