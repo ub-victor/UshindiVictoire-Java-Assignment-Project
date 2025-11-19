f9cc1d4bf823e9341e9f57d9a98318546cee6a60
ub-victor <victoireushindi371@gmail.com>
2025-11-19 17:13:38 +0200

    Inherintance

--- src/_27269/q6/Position.java
+++ src/_27269/q6/Position.java
@@ -1,5 +1,26 @@
 package _27269.q6;
 
-public class Position {
-    
+public class Position extends Contract {
+    private String positionName;
+    private double baseSalary;  // > 0
+
+    public Position(int id, String createdDate, String updatedDate,
+                    String employeeName, String employeeTIN, String department,
+                    String contractType, String startDate, String endDate,
+                    String positionName, double baseSalary)
+            throws PayrollException {
+        super(id, createdDate, updatedDate, employeeName, employeeTIN, department,
+                contractType, startDate, endDate);
+
+        if (positionName == null || positionName.trim().isEmpty())
+            throw new PayrollException("positionName required");
+        if (baseSalary <= 0)
+            throw new PayrollException("baseSalary must be > 0");
+
+        this.positionName = positionName;
+        this.baseSalary = baseSalary;
+    }
+
+    public String getPositionName() { return positionName; }
+    public double getBaseSalary() { return baseSalary; }
 }
